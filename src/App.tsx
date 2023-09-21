80452af74f3a953fcb0cd27d08c13eb4c4769679
agsjorda <agsjorda@gmail.com>
2023-09-21 23:28:31 +0800

    added components folder, InputField component and Todo model

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
+import InputField from "./components/InputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
+	const [todo, setTodo] = useState<string>("");
+	const [todos, setTodos] = useState<Todo[]>([]);
+
+	const handleAdd = (e: React.FormEvent) => {
+		e.preventDefault();
+
+		if (todo) {
+			setTodos([...todos, { id: Date.now(), todo, isDone: false }]);
+			setTodo("");
+		}
+	};
+
+	console.log(todo);
+
 	return (
-		<div className="App">
-			<h1>Taskify</h1>
+		<div className="App bg-indigo-600">
+			<h1 className="uppercase text-4xl my-8 text-white z-10 text-center md:my-4 mx-0 md:text-[42px]">
+				Taskify
+			</h1>
+			<InputField todo={todo} setTodo={setTodo} handleAdd={handleAdd} />
 		</div>
 	);
 };
